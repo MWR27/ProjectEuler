afd6a6a48e6954ad24c45bce2a1e9cd9bb3bfcf2
MWR27 <64335495+MWR27@users.noreply.github.com>
2023-10-29 22:14:07 -0400

    Added public Access Modifiers

--- ProjectEulerProblem1.java
+++ ProjectEulerProblem1.java
@@ -3,11 +3,11 @@ public class ProjectEulerProblem1 {
       problem1();
    }
    
-   static void problem1() {
+   public static void problem1() {
       System.out.println(sumOfMultiplesOf(3, 1000) + sumOfMultiplesOf(5, 1000) - sumOfMultiplesOf(15, 1000));
    }
    
-   static int sumOfMultiplesOf(int factor, int limit) {
+   public static int sumOfMultiplesOf(int factor, int limit) {
       limit -= 1;
       int n = limit - limit % factor;
       return n * (n + factor) / (2 * factor);
