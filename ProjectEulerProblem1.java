ff93b96a502d0b8be82c9ea565f3a1c83ef0fddc
MWR27 <64335495+MWR27@users.noreply.github.com>
2023-10-29 22:02:55 -0400

    Add files via upload

---
+++ ProjectEulerProblem1.java
@@ -0,0 +1,15 @@
+public class ProjectEulerProblem1 {
+   public static void main(String[] args) {
+      problem1();
+   }
+   
+   static void problem1() {
+      System.out.println(sumOfMultiplesOf(3, 1000) + sumOfMultiplesOf(5, 1000) - sumOfMultiplesOf(15, 1000));
+   }
+   
+   static int sumOfMultiplesOf(int factor, int limit) {
+      limit -= 1;
+      int n = limit - limit % factor;
+      return n * (n + factor) / (2 * factor);
+   }
+}
